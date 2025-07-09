c7034491b200b2b217c12c70ca40264f088bf732
Rodrigo Borba <rodrigovborba@gmail.com>
2025-07-09 14:00:25 -0300

    updating tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const mockUserModel = {
+    create: jest.fn(),
+    find: jest.fn(),
+    findById: jest.fn(),
+    findByIdAndUpdate: jest.fn(),
+    findByIdAndDelete: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getModelToken(User.name),
+          useValue: mockUserModel,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
