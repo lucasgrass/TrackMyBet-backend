51fd4ad322a13009b955b0e0e95afb6b62ab8bc2
Rodrigo Borba <rodrigovborba@gmail.com>
2025-07-09 13:41:17 -0300

    updating tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,14 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UsersSchema } from './schemas/user.schema';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+          MongooseModule.forFeature([{ name: User.name, schema: UsersSchema }]),
+        ],
       controllers: [UsersController],
       providers: [UsersService],
+      exports: [UsersService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
