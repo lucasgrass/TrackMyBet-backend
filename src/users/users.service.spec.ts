afe935dcf511c6637cfff0ccc7a23b718f40e4f3
Rodrigo Borba <rodrigovborba@gmail.com>
2025-07-09 13:47:09 -0300

    updating tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,11 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UsersSchema } from './schemas/user.schema';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRoot('mongodb://localhost:27017/trackMyBet'),
+        MongooseModule.forFeature([{ name: User.name, schema: UsersSchema }]),
+      ],
       providers: [UsersService],
     }).compile();
 
